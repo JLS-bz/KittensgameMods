5c78d935a4a9d7f338d784702a49f9238132a2c5
JLS-bz <60501166+JLS-bz@users.noreply.github.com>
2026-04-10 04:56:16 +0000

    Deploying to gh-pages from @ JLS-bz/KittensgameMods@719dd9ce02951f924a0d3cffc27f20fee36fc7e6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -20,8 +20,8 @@ ninja.data = [{
           title: 'Kitten Turbo Speed',
           description: "Set custom game speed multipliers (1x, 25x, 50x, 75x, 100x) to accelerate your gameplay",section: "Mods",handler: () => {
               window.location.href = "/KittensgameMods/mods/1_mod/";
-            },},{id: "mods-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",section: "Mods",handler: () => {
+            },},{id: "mods-automation-panel",
+          title: 'Automation Panel',
+          description: "Adds a third panel tab called &quot;Automation&quot;, to the right of Log and Queue. Must be loaded first before other automation script mods that depend on it.",section: "Mods",handler: () => {
               window.location.href = "/KittensgameMods/mods/2_mod/";
             },},];
